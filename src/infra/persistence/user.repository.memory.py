632c53f5a79381b3c15f8af978c9a6b61dbb9df4
Vinicius Silva Barros <viniciussilvabarros05@hotmail.com>
2024-06-17 15:27:07 -0300

    feat: add memories persistence

---
+++ src/infra/persistence/user.repository.memory.py
@@ -0,0 +1,19 @@
+from core.repository.iuserRepository import IUserRepository
+from core.entity.user import User
+
+
+class EventRepositoryMemory(IUserRepository):
+    def __init__(self, repository: IUserRepository):
+        self.__repository = repository
+
+    async def create(self, user: User):
+        return await self.__repository.create(user)
+
+    async def getById(self, id: str, ) -> User:
+        return await self.__repository.getById(id)
+
+    async def update(self, user: User) -> None:
+        return await self.__repository.update(user)
+
+    async def delete(self, id: str) -> None:
+        return await self.__repository.delete(id)
